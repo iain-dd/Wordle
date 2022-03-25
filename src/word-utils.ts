675dbaf1b45d656d961b369e4ef3785439c5309a
iain-dd <iain.dorandesbrisay@gmail.com>
2022-03-25 18:11:34 -0400

    adding edge cases

--- src/word-utils.ts
+++ src/word-utils.ts
@@ -17,15 +17,54 @@ export function computeGuess(
   answerString: string
 ): LetterState[] {
   const result: LetterState[] = [];
+
+  if (guess.length !== answerString.length) {
+    return result;
+  }
   const guessArray = guess.split("");
+
   const answerArray = answerString.split("");
 
+  const answerLetterCount: Record<string, number> = {};
+
   guessArray.forEach((letter, index) => {
+    const currentAnswerLetter = answerArray[index];
+
+    answerLetterCount[currentAnswerLetter] = answerLetterCount[
+      currentAnswerLetter
+    ]
+      ? answerLetterCount[currentAnswerLetter] + 1
+      : 1;
+
     if (letter === answerArray[index]) {
       result.push(LetterState.Match);
     } else if (answerArray.includes(letter)) {
       result.push(LetterState.Present);
     } else result.push(LetterState.Miss);
+  });
+
+  result.forEach((curResult, resultIndex) => {
+    if (curResult !== LetterState.Present) {
+      return;
+    }
+
+    const guessLetter = guessArray[resultIndex];
+
+    answerArray.forEach((currentAnswerLetter, answerIndex) => {
+      if (currentAnswerLetter !== guessLetter) {
+        return;
+      }
+
+      if (result[answerIndex] === LetterState.Match) {
+        result[resultIndex] = LetterState.Miss;
+      }
+
+      if (answerLetterCount[guessLetter] <= 0) {
+        result[resultIndex] = LetterState.Miss;
+      }
+    });
+
+    answerLetterCount[guessLetter]--;
   });
   return result;
 }
