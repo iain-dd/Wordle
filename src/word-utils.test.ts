3865bef2ee4b616230852e7fae32bf2d6f454565
iain-dd <iain.dorandesbrisay@gmail.com>
2022-03-25 17:51:31 -0400

    basic compute guess (no edge cases)

--- src/word-utils.test.ts
+++ src/word-utils.test.ts
@@ -1,8 +1,51 @@
 import { describe, expect, it } from "vitest";
-import { getRandomWord } from "./word-utils";
+import { getRandomWord, computeGuess, LetterState } from "./word-utils";
 
-describe("Word-Utils", () => {
+describe("getRandomWord", () => {
   it("Random Word", () => {
     expect(getRandomWord()).toBeTruthy();
+    expect(getRandomWord().length).toEqual(5);
+  });
+});
+
+describe("computeGuess", () => {
+  it("works with match and present", () => {
+    expect(computeGuess("boost", "basic")).toEqual([
+      LetterState.Match,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Present,
+      LetterState.Miss,
+    ]);
+  });
+
+  it("works with all matches", () => {
+    expect(computeGuess("boost", "boost")).toEqual([
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+    ]);
+  });
+
+  it("works with all miss", () => {
+    expect(computeGuess("boost", "guard")).toEqual([
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+    ]);
+  });
+
+  it("works with all miss", () => {
+    expect(computeGuess("solid", "boost")).toEqual([
+      LetterState.Present,
+      LetterState.Match,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+    ]);
   });
 });
